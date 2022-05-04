d4010c02f550132368ac13226fb023a8597614f7
linzhiqiu <zl279@cornell.edu>
2022-05-04 02:45:56 -0400

    [in progress] two head debug

--- lib/core/function.py
+++ lib/core/function.py
@@ -66,8 +66,7 @@ def train_two_head(config, epoch, num_epoch, epoch_iters, base_lr, num_iters,
         for i in range(images_t_1.shape[0]):
             time_indices_1.append(counter)
             counter += 1
-        print(f"Max of label_0 {torch.max(labels_t_0)} and 1 is {torch.max(labels_t_1)}")
-        loss, _ = model(images, labels_t_0, labels_t_1, time_indices_0, time_indices_1)
+        loss, _, _ = model(images, labels_t_0, labels_t_1, time_indices_0, time_indices_1)
         loss = loss.mean()
 
         if dist.is_distributed():
